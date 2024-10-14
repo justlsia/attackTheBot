54b5dbc90b8eb11e7a591b23fd3c60561fb9c296
Illi <107838600+justlsia@users.noreply.github.com>
2024-10-14 23:13:17 +0200

    Update joueur.dart

--- joueur.dart
+++ joueur.dart
@@ -11,25 +11,26 @@ class Joueur {
     int sante           = 0;
 
   // Méthodes
+
   void displayPlayerInfo() {
     print("État du joueur ${this.pseudo} : force = ${this.force} - santé = ${this.sante} - score = ${this.score}");
   }
 
   void attackBot(Bot bot) {
-    // Appel de la fonction lancerDes pour déterminer la force de l'attaque
+    // Appeler la fonction lancerDes pour déterminer la force de l'attaque
     int scoreDes = lancerDes(this.pseudo);
-    // Calcul des dégâts en multipliant le score des dés par la force du Bot
+    // Calculer les dégâts en multipliant le score des dés par la force du Bot
     int degats = scoreDes * bot.force;
-    // Affichage de l'action d'attaque
+    // Afficher l'action d'attaque
     print("${this.pseudo} assène un coup sur le bot avec une force de ${degats}."); 
-    // Réduction de la santé du Bot en fonction des dégâts
+    // Réduir la santé du Bot en fonction des dégâts
     bot.sante -= degats;
-    // Vérification si la santé du Bot est négative
+    // Vérifier si la santé du Bot est négative
     if (bot.sante < 0) { 
-      // Réinitialisation de la santé à zéro si elle est négative
+      // Réinitialiser la santé à zéro si elle est négative
       bot.sante = 0; 
     }
-    // Affichage de la santé restante du Bot
+    // Afficher la santé restante du Bot
     print("Bot - Santé ${bot.sante}%"); 
   }
 
